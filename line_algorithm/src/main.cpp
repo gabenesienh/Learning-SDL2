dd456c4bcd35284c4e224ee002e9fb9d200ff2b4
gabenesienh <gabrielenes@ienh.com.br>
2024-12-19 11:21:27 -0300

    Move line algorithm to separate file with header
    Add right click to change width in line algorithm testing app
    
    Algorithm still uses floating point math, which is good enough, but I'll
    look into changing it to integer-only in the future to improve
    performance

--- line_algorithm/src/main.cpp
+++ line_algorithm/src/main.cpp
@@ -1,14 +1,10 @@
-// Line drawing algorithm
-// Made for use in Worseprite
-
-// TODO: implement bresenham's algorithm
+// Line algorithm test app
 
 #include <SDL2/SDL.h>
 #include <iostream>
-#include <cmath>
+#include "linealg.h"
 
 using std::cin, std::cout;
-using std::abs, std::max;
 
 bool init();
 bool loop();
@@ -51,15 +47,27 @@ bool loop() {
 				mousePos = {event.button.x, event.button.y};
 				break;
 			case SDL_MOUSEBUTTONDOWN:
-				if (event.button.button == SDL_BUTTON_LEFT) {
-					// Set the origin point for the drawn line
-					anchorPos = {event.button.x, event.button.y};
-					mousePressed = true;
+				switch (event.button.button) {
+					case SDL_BUTTON_LEFT:
+						// Set the origin point for the drawn line
+						anchorPos = {event.button.x, event.button.y};
+						mousePressed = true;
+						break;
+					case SDL_BUTTON_RIGHT:
+						lineBrush->w = 5;
+						lineBrush->h = 5;
+						break;
 				}
 				break;
 			case SDL_MOUSEBUTTONUP:
-				if (event.button.button == SDL_BUTTON_LEFT) {
-					mousePressed = false;
+				switch (event.button.button) {
+					case SDL_BUTTON_LEFT:
+						mousePressed = false;
+						break;
+					case SDL_BUTTON_RIGHT:
+						lineBrush->w = 1;
+						lineBrush->h = 1;
+						break;
 				}
 				break;
 			case SDL_QUIT:
@@ -71,37 +79,7 @@ bool loop() {
 	SDL_FillRect(winSurface, NULL, SDL_MapRGB(winSurface->format, 0, 0, 0));
 
 	if (mousePressed) {
-		// Draw a line to the cursor
-
-		// Start and end coordinates for the line
-		Sint32 x0 = anchorPos.x;
-		Sint32 y0 = anchorPos.y;
-		Sint32 x1 = mousePos.x;
-		Sint32 y1 = mousePos.y;
-
-		// Delta values (how far the cursor is from the anchor point)
-		Sint32 dx = x1 - x0;
-		Sint32 dy = y1 - y0;
-
-		// Get the magnitude of the largest delta of the line
-		// This makes it easy to adjust for all octants
-		Sint32 step = max(abs(dx), abs(dy));
-
-		for (int i = 0; i <= step; i++) {
-			// Avoid division by 0 error
-			if (step == 0) break;
-
-			//double m = static_cast<double>(dy) / dx;
-
-			// One of these will be 1, the other will be the slope
-			double stepX = static_cast<double>(dx) / step;
-			double stepY = static_cast<double>(dy) / step;
-
-			lineBrush->x = x0 + i * stepX;
-			lineBrush->y = y0 + i * stepY;
-
-			SDL_FillRect(winSurface, lineBrush, SDL_MapRGB(winSurface->format, 255, 255, 255));
-		}
+		drawLine(winSurface, lineBrush, SDL_MapRGB(winSurface->format, 255, 255, 255), anchorPos.x, anchorPos.y, mousePos.x, mousePos.y);
 	}
 
 	SDL_UpdateWindowSurface(window);
